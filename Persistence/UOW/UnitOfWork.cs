4223d6699aa1d19360d5c324c6e1841289acce5e
Kolya Voitekhovich <kolya.vaitihovich09@gmail.com>
2022-09-07 11:40:07 +0300

    Add Unit of work

---
+++ Persistence/UOW/UnitOfWork.cs
@@ -0,0 +1,62 @@
+﻿using Application.Common.Interfaces.Contexts;
+using Application.Common.Interfaces.Repositories;
+using Persistence.Repositories;
+
+namespace Persistence.UOW
+{
+    public class UnitOfWork
+    {
+        private readonly IApplicationDbContext _context;
+
+        private IAddressRepository _addressRepository;
+        private IDepartmentRepository _departmentRepository;
+        private IEmployeeRepository _employeeRepository;
+        private IOrganizationRepository _organizationRepository;
+        private IPositionRepository _positionRepository;
+
+        public UnitOfWork(IApplicationDbContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public IAddressRepository AddressRepository
+        {
+            get
+            {
+                return _addressRepository ??= new AddressRepository(_context);
+            }
+        }
+
+        public IDepartmentRepository DepartmentRepository
+        {
+            get
+            {
+                return _departmentRepository ??= new DepartmentRepository(_context);
+            }
+        }
+
+        public IEmployeeRepository EmployeeRepository
+        {
+            get
+            {
+                return _employeeRepository ??= new EmployeeRepository(_context);
+            }
+        }
+
+        public IOrganizationRepository OrganizationRepository
+        {
+            get
+            {
+                return _organizationRepository ??= new OrganizationRepository(_context);
+            }
+        }
+
+        public IPositionRepository PositionRepository
+        {
+            get
+            {
+                return _positionRepository ??= new PositionRepository(_context);
+            }
+        }
+    }
+}
