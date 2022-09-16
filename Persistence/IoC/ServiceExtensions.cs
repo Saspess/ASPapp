0ab65254744006a31254a19418e314ff93efde94
Kolya Voitekhovich <kolya.vaitihovich09@gmail.com>
2022-09-16 23:29:35 +0300

    Modify IoC for Persistence project

--- Persistence/IoC/ServiceExtensions.cs
+++ Persistence/IoC/ServiceExtensions.cs
@@ -1,15 +1,23 @@
-﻿using Persistence.Contexts;
+﻿using Application.Common.Interfaces.Contexts;
+using Application.Common.Interfaces.UOW;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System.Reflection;
+using Persistence.Contexts;
+using Persistence.UOW;
 
 namespace Persistence.IoC
 {
-    public static class ServiceExtensions 
+    public static class ServiceExtensions
     {
         public static void ConfigureSqlContext(this IServiceCollection services, IConfiguration configuration) =>
             services.AddDbContext<ApplicationDbContext>(opts =>
             opts.UseSqlServer(configuration.GetConnectionString("sqlConnection")));
+
+        public static void ConfigureUnitOfWork(this IServiceCollection services) =>
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+        public static void ConfigureDbContext(this IServiceCollection services) =>
+            services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
     }
 }
