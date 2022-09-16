b33be6793852657fdf0527e0391edc4c04c12121
Kolya Voitekhovich <kolya.vaitihovich09@gmail.com>
2022-09-16 23:31:29 +0300

    Add IoC for Application project

---
+++ Application/IoC/ServicesExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Application.Common.Interfaces.Services;
+using Application.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application.IoC
+{
+    public static class ServicesExtensions
+    {
+        public static void ConfigureServices(this IServiceCollection services)
+        {
+            services.AddScoped<IAddressService, AddressService>();
+            services.AddScoped<IDepartmentService, DepartmentSevice>();
+            services.AddScoped<IEmployeeService, EmployeeService>();
+            services.AddScoped<IOrganizationService, OrganizationService>();
+            services.AddScoped<IPositionService, PositionService>();
+        }
+    }
+}
