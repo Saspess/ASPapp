0cf46e4a1a1ed84d8fd50c26c8aa0e3ff6c16f74
Kolya Voitekhovich <kolya.vaitihovich09@gmail.com>
2022-09-16 23:24:21 +0300

    Add interface for Unit of work

---
+++ Application/Common/Interfaces/UOW/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using Application.Common.Interfaces.Repositories;
+
+namespace Application.Common.Interfaces.UOW
+{
+    public interface IUnitOfWork
+    {
+        IAddressRepository AddressRepository { get; }
+        IDepartmentRepository DepartmentRepository { get; }
+        IEmployeeRepository EmployeeRepository { get; }
+        IOrganizationRepository OrganizationRepository { get; }
+        IPositionRepository PositionRepository { get; }
+    }
+}
